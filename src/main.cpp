92bc8cb40fd60ec73269cc046aa9e730e2627450
Fernando Silveira <fsilveira@gmail.com>
2021-10-11 19:04:27 -0300

    Implemented --help argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
+#include <err.h>
+#include <stdlib.h>
+
 #include <iostream>
+
+#include <boost/program_options.hpp>
+
+using std::cout;
+using std::endl;
+using std::exception;
+
+namespace po = boost::program_options;
+
+static void
+run()
+{
+}
 
 int
 main(int argc, char *argv[])
 {
-	return 0;
+	// Declare the supported options.
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", "produce help message")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("help")) {
+		cout << desc << endl;
+		return EXIT_SUCCESS;
+	}
+
+	try {
+		run();
+	} catch (exception &e) {
+		warnx("unhandled exception: %s", e.what());
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
